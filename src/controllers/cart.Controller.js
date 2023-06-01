642fa41c9592c3488b9a0c4815e013cb2cb10675
JulSarmiento <jasa1999@hotmail.com>
2023-06-01 16:39:54 -0500

    carrito funcionado correctamente

---
+++ src/controllers/cart.Controller.js
@@ -0,0 +1,54 @@
+import {Cart, CartItem, Product} from "../models/index.js";
+
+export const getCart = async (req, res, next) => {
+  const { storeId } = req.params;
+  const { userId } = req.query; // cambiar despues por jwt
+  try {
+    const cart = await Cart.findOrCreate({
+      where: { userId, storeId },
+      include: [Product],
+    });
+
+    res.status(200).json(cart);
+
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const addProductToCart = async (req, res, next) => {
+  const { storeId } = req.params;
+  const { userId } = req.query; // cambiar despues por jwt
+  const { productId, count } = req.body;
+  try {
+    const [cart] = await Cart.findOrCreate({
+      where: { userId, storeId },
+      include: [Product],
+    });
+
+    const [cartItem] = await CartItem.findOrCreate({
+      where: { cartId: cart.id, productId }
+    });
+
+    if(count === 0) {
+      await cartItem.destroy();
+
+      res.status(200).json({
+        success: true,
+        message: "Product removed from cart"
+      });
+      return;
+    }
+
+    cartItem.count = count;
+    await cartItem.save();
+
+    res.status(200).json({
+      success: true,
+      data: cartItem
+    });
+
+  } catch (error) {
+    next(error);
+  }
+}
--- src/routes/stores.routes.js
+++ src/routes/stores.routes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { getStores, createStore, getStoreByParam , updateStore, deleteStore} from "../controllers/stores.controller.js";
-
+import { getCart, addProductToCart } from "../controllers/cart.Controller.js";
 const router = express.Router();
 
 router.get("/", getStores);
@@ -9,4 +9,6 @@ router.get("/:param", getStoreByParam);
 router.patch("/:id", updateStore);
 router.delete("/:id", deleteStore);
 
+router.get("/:storeId/cart", getCart);
+router.post("/:storeId/cart", addProductToCart);
 export default router;
